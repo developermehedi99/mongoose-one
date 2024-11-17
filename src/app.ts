eb2f8e7ebfb8ce832b7d70b9c37758f43dcf8efa
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-11-17 19:27:12 +0600

    project set uping

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
-const express = require('express')
-const app = express()
+import express, { Application, Request, Response } from "express"
+import cors from "cors"
+const app:Application = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+// parser middle ware
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
 })
 
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose');
-
-
+import app from "./app";
+import config from "./app/config";
+import mongoose from "mongoose";
 
 async function main() {
-  await mongoose.connect(process.env.DATABASE_URL);
+  try {
+    await mongoose.connect(config.database_url as string);
 
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
